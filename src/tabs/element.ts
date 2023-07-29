e01c5fe3543d8d9571eb26171b3b868e21f04273
“chrisshank” <chris.shank.23@gmail.com>
2023-07-28 21:27:30 -0700

    refactor tabs

--- src/tabs/element.ts
+++ src/tabs/element.ts
@@ -2,59 +2,58 @@ import { render } from 'lit';
 import { renderTabs, Tab } from './template';
 
 export class TheseTabs extends HTMLElement {
-  _tabs: Tab[] = [];
-
   constructor() {
     super();
 
-    this.onClick = this.onClick.bind(this);
-
-    const tabEls = this.querySelectorAll('[slot="tab"]');
-    this._tabs = Array.from(tabEls).map((tab) => ({
-      id: tab.getAttribute('tab-id') || '',
-      label: tab.querySelector('span')?.innerText ?? '',
-      closeable: tab.getAttribute('closable') === 'true',
-    }));
-  }
-  connectedCallback() {
-    this.addEventListener('click', this.onClick);
+    this.addEventListener('click', this);
   }
 
-  disconnectedCallback() {
-    this.removeEventListener('click', this.onClick);
-  }
-
+  #tabs: Tab[] | null = null;
   get tabs(): Tab[] {
-    return this._tabs;
+    if (this.#tabs === null) {
+      const tabEls = this.querySelectorAll('[slot="tab"]');
+      this.#tabs = Array.from(tabEls).map((tab) => ({
+        id: tab.getAttribute('tab-id') || '',
+        label: tab.querySelector('span')?.innerText ?? '',
+        closeable: tab.getAttribute('closable') === 'true',
+      }));
+    }
+    return this.#tabs;
   }
 
   set tabs(tabs: Tab[]) {
-    this._tabs = tabs;
+    this.#tabs = tabs;
     this.render();
   }
 
-  get activeTab(): string {
+  get activeTab() {
     return this.getAttribute('active-tab') || '';
   }
 
-  onClick(e: MouseEvent) {
+  set activeTab(value: string) {
+    this.setAttribute('active-tab', value);
+  }
+
+  handleEvent(e: Event) {
     if (e.target === null || !(e.target instanceof HTMLElement)) return;
 
-    const tabEl = e.target.closest('[slot="tab"]');
-    const closeButtonEl = e.target.closest('button[slot="tab-close-button"]');
-    if (closeButtonEl !== null && tabEl !== null) {
-      const tabId = tabEl.getAttribute('tab-id');
-      this.tabs = this.tabs.filter((tab) => tab.id !== tabId);
-    } else if (tabEl !== null) {
-      const tabId = tabEl.getAttribute('tab-id');
-      if (tabId === null) return;
+    if (e.type === 'click') {
+      const tabEl = e.target.closest('[slot="tab"]');
+      const closeButtonEl = e.target.closest('button[slot="tab-close-button"]');
+      if (closeButtonEl !== null && tabEl !== null) {
+        const tabId = tabEl.getAttribute('tab-id');
+        this.#tabs = this.tabs.filter((tab) => tab.id !== tabId);
+      } else if (tabEl !== null) {
+        const tabId = tabEl.getAttribute('tab-id');
+        if (tabId === null) return;
 
-      const activeTab = this.querySelector('[slot="tab"][active="true"]');
-      if (activeTab === null) return;
+        const activeTab = this.querySelector('[slot="tab"][active="true"]');
+        if (activeTab === null) return;
 
-      activeTab.setAttribute('active', 'false');
-      tabEl.setAttribute('active', 'true');
-      this.setAttribute('active-tab', tabId);
+        activeTab.setAttribute('active', 'false');
+        tabEl.setAttribute('active', 'true');
+        this.setAttribute('active-tab', tabId);
+      }
     }
   }
 
@@ -66,7 +65,7 @@ export class TheseTabs extends HTMLElement {
       this.replaceChildren();
       this.hasRendered = true;
     }
-    render(renderTabs({ tabs: this._tabs, activeTab: this.activeTab }), this);
+    render(renderTabs({ tabs: this.#tabs || [], activeTab: this.activeTab }), this);
   }
 }
 
