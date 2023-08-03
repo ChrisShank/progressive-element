e836d350dc8ce8ca31369f8283ff9cb1e2377fc3
“chrisshank” <chris.shank.23@gmail.com>
2023-08-02 22:24:01 -0700

    update approach to event delegation

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,48 @@
 // import './tabs/element.ts'
 
-// Any string is valid, but we can provide auto-complete for built-in event.
-type AnyEventType = keyof HTMLElementEventMap | (string & {});
+interface Intention {
+  intention: string;
+  target: HTMLElement;
+}
 
-function Listener(type: AnyEventType, selectors: string) {
-  return (target: ProgressiveElement, methodName: string) => {
-    const c = target.constructor as typeof ProgressiveElement;
-    if (c.delegatedEvents.get(type) === undefined) {
-      c.delegatedEvents.set(type, []);
+function findClosestIntention(event: Event): Intention | Record<string, never> {
+  if (event.target instanceof Element) {
+    const target = event.target.closest(`[on\\:${event.type}]`) as HTMLElement | null;
+
+    if (target !== null) {
+      const intention = target.getAttribute(`on:${event.type}`)!;
+      return { intention, target };
     }
-
-    c.delegatedEvents.get(type)?.push({ methodName, selectors });
-  };
-}
-
-interface DelegatedEventListenerConfig {
-  methodName: string;
-  selectors: string;
-}
-
-type DelegatedEventListenersMap = Map<AnyEventType, DelegatedEventListenerConfig[]>;
-
-type DelegatedEventListener = (event: Event, el: Element) => void;
-
-class ProgressiveElement extends HTMLElement {
-  static delegatedEvents: DelegatedEventListenersMap = new Map();
-
-  static properties = new Map<string, any>();
-
-  static get observedAttributes() {
-    return [];
   }
+  return {};
+}
+
+class MyElement extends HTMLElement {
+  count: HTMLElement = this.querySelector('#count')!;
 
   constructor() {
     super();
-    const c = this.constructor as typeof ProgressiveElement;
-    for (const event of c.delegatedEvents.keys()) {
-      this.addEventListener(event, this);
-    }
+
+    this.addEventListener('click', this);
   }
 
   handleEvent(event: Event) {
-    const c = this.constructor as typeof ProgressiveElement;
-    const listeners = c.delegatedEvents.get(event.type);
+    const { intention } = findClosestIntention(event);
 
-    if (listeners === undefined || event.target === null || !(event.target instanceof Element)) {
-      // TODO: throw error about `delegatedEvents` being modified
-      return;
-    }
+    if (intention === undefined) return;
 
-    for (const { selectors, methodName } of listeners) {
-      const selectedElement = event.target.closest(selectors);
-      if (selectedElement !== null) {
-        const method = this[methodName as keyof ProgressiveElement] as DelegatedEventListener;
+    event.stopPropagation(); // Stop event propagation of all events that have an intention
 
-        if (typeof method !== 'function') {
-          // TODO: throw error about method not being a function
-          return;
-        }
-
-        method.call(this, event, selectedElement);
+    switch (intention) {
+      case 'INCREMENT': {
+        this.count.innerText = (Number(this.count.innerText) + 1).toString();
+        break;
+      }
+      case 'DECREMENT': {
+        this.count.innerText = (Number(this.count.innerText) - 1).toString();
+        break;
       }
     }
-  }
-}
-
-class MyElement extends ProgressiveElement {
-  @Listener('click', 'button, [role="button"]')
-  onButtonClick(_event: Event, element: HTMLElement) {
-    element.innerText = (Number(element.innerText) + 1).toString();
   }
 }
 
