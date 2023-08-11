8d9fde1fb76b0cf3ba4fd425631d81532147f5b4
“chrisshank” <chris.shank.23@gmail.com>
2023-08-11 15:57:32 -0700

    scaffold app

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,10 @@
 import { defineConfig, Plugin } from 'vite';
-import { renderToString } from './src/render-to-string';
-import { renderTabs } from './src/tabs/template';
 
 const htmlPlugin = (): Plugin => {
   return {
     name: 'html-transform',
     transformIndexHtml(html) {
-      const tabs = renderToString(
-        renderTabs(
-          {
-            tabs: [
-              { id: 'foo', label: 'Foo' },
-              { id: 'bar', label: 'Bar', closeable: true },
-              { id: 'baz', label: 'Baz', closeable: true },
-              { id: 'fooBar', label: 'FooBar', closeable: true },
-            ],
-            activeTab: 'foo',
-          },
-          { isServerRendered: true }
-        )
-      );
-      return html.replace('RENDER_TABS', tabs);
+      return;
     },
   };
 };
