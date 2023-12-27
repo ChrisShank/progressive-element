66d86c9e9a988e02f6c4e2c6d3aab9bd03269759
Chris Shank <chris.shank.23@gmail.com>
2023-12-26 16:19:30 -0800

    Update progressive-element.ts

--- src/progressive-element.ts
+++ src/progressive-element.ts
@@ -3,8 +3,11 @@ interface Intention {
   target: Element;
 }
 
+// Any string is valid, but we can provide auto-complete for built-in event.
+type AnyEventType = keyof HTMLElementEventMap | (string & {});
+
 export interface AnyEvent {
-  type: string;
+  type: AnyEventType;
   [key: string]: any;
 }
 
@@ -37,19 +40,6 @@ export function keyboardModifiers(event: AnyEvent) {
   return '';
 }
 
-export class PropertyChangeEvent {
-  readonly type = 'property-change';
-
-  constructor(
-    readonly name: string,
-    readonly oldValue: string | null,
-    readonly newValue: string | null
-  ) {}
-}
-
-// Any string is valid, but we can provide auto-complete for built-in event.
-type AnyEventType = keyof HTMLElementEventMap | (string & {});
-
 function kebabize(str: string) {
   return str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? '-' : '') + $.toLowerCase());
 }
@@ -59,30 +49,13 @@ export class ProgressiveElement extends HTMLElement implements EventListenerObje
     customElements.define(kebabize(this.name), this);
   }
 
-  static properties?: Record<string, any>;
-
-  static get observedAttributes() {
-    if (this.properties === undefined) {
-      return undefined;
-    }
-
-    return Object.keys(this.properties);
-  }
-
   static delegatedEvents?: AnyEventType[];
 
   constructor() {
     super();
 
-    // TODO initialize properties for properties
     const constructor = this.constructor as typeof ProgressiveElement;
-
     constructor.delegatedEvents?.forEach((event) => this.addEventListener(event, this));
-  }
-
-  // TODO: parse attributes into properties
-  attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
-    this.handleEvent(new PropertyChangeEvent(name, oldValue, newValue));
   }
 
   // @ts-ignore
