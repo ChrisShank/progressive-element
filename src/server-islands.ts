751b099b11fe6d4a3462517f35460776a56d61b1
“chrisshank” <chris.shank.23@gmail.com>
2024-08-25 18:35:13 -0700

    simplify server island

--- src/server-islands.ts
+++ src/server-islands.ts
@@ -1,27 +1,18 @@
 export class ServerIsland extends HTMLElement {
-  static tagName = 'server-island';
-
-  static register() {
-    customElements.define(this.tagName, this);
-  }
-
-  #html = '';
-
   async connectedCallback() {
     const src = this.getAttribute('src');
 
     if (!src) return;
 
-    if (this.#html !== '') {
-      const response = await fetch(src);
+    const response = await fetch(src);
 
-      if (response.status === 200 && response.headers.get('content-type') === 'text/html') {
-        this.#html = await response.text();
-      }
+    if (response.status === 200 && response.headers.get('content-type') === 'text/html') {
+      const html = await response.text();
+      const frag = document.createRange().createContextualFragment(html);
+      this.textContent = ''; // Clear loading content
+      this.appendChild(frag);
     }
-
-    const frag = document.createRange().createContextualFragment(this.#html);
-    this.textContent = ''; // Clear loading content
-    this.appendChild(frag);
   }
 }
+
+customElements.define('server-island', ServerIsland);
